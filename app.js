a20bc3a066b3446f1327a21b0e1fecd35b66f56c
sanjayan <sanjayan0721@gmail.com>
2024-03-08 11:41:03 +0530

    first

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/v1", mainRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5173;
 
 const start = async () => {
     try {
